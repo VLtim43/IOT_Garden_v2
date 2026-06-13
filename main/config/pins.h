d37bd7e79c9508df764cad04de3227e9f95c5f27
VLtim43 <fernandoreal.valentim@gmail.com>
2026-06-13 15:02:51 -0300

    fixes'

--- main/config/pins.h
+++ main/config/pins.h
@@ -5,11 +5,11 @@
 #define OLED_I2C_SDA_GPIO 21
 #define OLED_I2C_SCL_GPIO 22
 
-// Digital photoresistor module DO pin.
-#define LIGHT_SENSOR_DO_GPIO 32
+// Photoresistor module DO pin.
+#define LIGHT_SENSOR_DO_GPIO 34
 #define LIGHT_SENSOR_DAY_LEVEL 0
 
 // DHT11 sensor data pin.
-#define DHT11_DATA_GPIO 4
+#define DHT11_DATA_GPIO 27
 
 #endif
